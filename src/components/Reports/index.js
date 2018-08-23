f63b0e9ada569e1d76ac37ff12cc7e776d5d5db8
chacon977 <andychacon@Andys-MacBook-Air.local>
2018-08-22 19:15:32 -0600

    graficos

--- src/components/Reports/index.js
+++ src/components/Reports/index.js
@@ -1,5 +1,9 @@
 // Dependencies
+
 import React, { Component } from 'react';
+import { reactjs } from 'react-chartjs';
+import { Chart, Axis, Series, Tooltip, Cursor, Bar } from "react-charts";
+//import 'react-chartjs' from './node_modules/'
 //import PropTypes from 'prop-types';
 //CSS
 import '../global/css/General.css';
@@ -12,12 +16,40 @@ import items from '../../data/menu';
 
 class Reports extends Component {
 
-  render(){
-    return(
+
+  render() {
+    var BarChart = require("react-chartjs").Bar;
+
+    var data =
+      [
+        {
+          label: "Series 1",
+          data: [{ x: 1, y: 10 }, { x: 2, y: 10 }, { x: 3, y: 10 }]
+        },
+        {
+          label: "Series 2",
+          data: [{ x: 1, y: 10 }, { x: 2, y: 10 }, { x: 3, y: 10 }]
+        },
+        {
+          label: "Series 3",
+          data: [{ x: 1, y: 10 }, { x: 2, y: 10 }, { x: 3, y: 10 }]
+        }
+      ];
+
+    return (
       <div className="Content Body">
         <div className="row Body">
           <div className="col-3"><LeftMenu items={items} /></div>
-          <div className="col-9">Reports</div>
+          <div className="col-9">
+            <Chart data={data}>
+              <Axis primary type="ordinal" />
+              <Axis type="linear" min={0} max={0} stacked />
+              <Series type={Bar} />
+              <Cursor primary />
+              <Cursor />
+              <Tooltip />
+            </Chart>
+          </div>
         </div>
       </div>
     );
