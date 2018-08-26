874fc843fa6d2dbbdb31b9d2550d368bb168acbe
dasoto <dsqskate@gmail.com>
2018-08-26 00:13:41 -0600

    coll

--- src/components/Home/Table.js
+++ src/components/Home/Table.js
@@ -8,50 +8,83 @@ import VideoModal from './VideoModal'
 import TextModal from './TextModal'
 import Collapse, {Panel} from 'rc-collapse';
 
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
+let p = [];
+
 
 class Table extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      names: []
+      names: [
+        {
+          "value": "",
+          "children": [
+            {"value": "1"},
+            {"value": "2"}
+          ]
+        }
+      ]
     }
   }
 
-  getDataFromCloud() {
+  componentDidMount() {
+    console.log("asda");
     let XMLParser = require('react-xml-parser');
-    let result;
     let dir = "https://interviewbotstorage.file.core.windows.net/interviews?sv=2017-11-09&ss=bfqt&srt=sco&sp=rwdlacup&se=2018-12-06T10:06:04Z&st=2018-08-25T02:06:04Z&spr=https&sig=5AGJ1NaX6JM97J167OUqXqWme3k1cLyvS%2Fu5wUqfKo4%3D&restype=directory&comp=list";
     let baseDir = "https://interviewbotstorage.file.core.windows.net/interviews";
     let lastDir = "?sv=2017-11-09&ss=bfqt&srt=sco&sp=rwdlacup&se=2018-12-06T10:06:04Z&st=2018-08-25T02:06:04Z&spr=https&sig=5AGJ1NaX6JM97J167OUqXqWme3k1cLyvS%2Fu5wUqfKo4%3D&restype=directory&comp=list";
 
-    axios.get(dir).then(response => {
+    let url = "https://interviewbotstorage.file.core.windows.net/interviews/pcass78@gmail.com/Test_7_4_2018_23_0_26/interview.webm?sv=2017-11-09&ss=bfqt&srt=sco&sp=rwdlacup&se=2018-12-06T10:06:04Z&st=2018-08-25T02:06:04Z&spr=https&sig=5AGJ1NaX6JM97J167OUqXqWme3k1cLyvS%2Fu5wUqfKo4%3D";
+
+
+    let result = axios.get(dir).then(response => {
       result = new XMLParser().parseFromString(response.data);
       result = result.getElementsByTagName('Name');
+      console.log("first inside");
       result.map((value, key) => {
-        axios.get(baseDir + "/" + value.value + lastDir).then(response => {
-          let folderDate = new XMLParser().parseFromString(response.data);
-          folderDate = folderDate.getElementsByTagName('Name');
-          result[key].folder = [];
-          folderDate.map((value) => {
-            result[key].folder.push(value);
+        let fold = axios.get(baseDir + "/" + value.value + lastDir).then(response => {
+          fold = new XMLParser().parseFromString(response.data);
+          fold = fold.getElementsByTagName('Name');
+          result[key].children = fold;
+          p.push(
+            <Panel header={`Employee Email: ${value.value}`} key={key} expandIcon={() => this.directories}>
+              <div>
+                <table className="table table-hover ">
+                  <thead>
+                  <tr>
+                    <th>Category</th>
+                    <th>Date</th>
+                    <th>Interview</th>
+                    <th>Interview Video</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  {
+                    fold.map((value, key) => {
+                      let text = value.value.split('_');
+                      return (
+                        <tr key={key}>
+                          <td>{text[0]}</td>
+                          <td>{text[2]+" - "+text[1]+" - "+text[3]}</td>
+                          <td><TextModal/></td>
+                          <td><a href={url}>eraasd</a></td>
+                        </tr>)
+                    })
+                  }
+                  </tbody>
+                </table>
+              </div>
+            </Panel>
+          );
+          this.setState({
+            names: result
           });
         });
-        this.setState({
-          names: result
-        });
+        console.log("second");
       });
-    })
-  }
-
-  componentDidMount() {
-    //console.log("will mount");
-    this.getDataFromCloud();
+    });
+    console.log("first");
   }
 
   componentWillMount() {
@@ -59,78 +92,61 @@ class Table extends Component {
   }
 
   shouldComponentUpdate(prevState, nextState) {
-    //console.log("should ",prevState,nextState);
-    //console.log(this.state.names, "     ", nextState.names);
     return this.state.names !== nextState.names
   }
 
 
   componentWillUpdate(nextProps, nextState) {
-    console.log("will update", nextState.names[0].folder);
+    //console.log("will update", nextState);
     this.setState({
       names: nextState.names
     });
-
   }
 
-  componentDidUpdate(currentProps, currentState) {
-    console.log("did update", this.state.names);
-    this.state.names.map((value, key) => {
-      console.log("PRUEBAAAAAAAAA", value);
-
-      console.log(value.folder);
-      //value.folder.map((value, key) => {
-      //console.log("aqaaaasssss", value);
-      //console.log(value);
-      //})
-    })
+  componentDidUpdate(prevProps, prevState) {
+    console.log("did update", this.state.names[0].children, "+++", this.state.names[1].children);
   }
 
   onChange = (activeKey) => {
-    this.setState({
-      activeKey,
+    this.state.names.map((value, key) => {
     });
   };
 
   getItems() {
     const items = [];
-    for (let i = 0, len = 3; i < len; i++) {
-      const key = i + 1;
+    this.state.names.map((value, key) => {
       items.push(
-        <Panel header={`This is panel header ${key}`} key={key} disabled={i === 0}>
-          <p>{text}</p>
+        <Panel header={`This is panel header ${value.value}`} key={key} expandIcon={() => this.directories}>
+          {
+            p
+          }
+
+          {
+            console.log("COMPONEEENT - -- - -:", this.state.names[key].children)
+          }
         </Panel>
-      );
-    }
+      )
+    });
+
+
     return items;
   }
 
-
   directories = () => {
-    this.state.names.map((value, key) => {
-      console.log("PRUEBAAAAAAAAA", value);
-
-      console.log(value.folder);
-      value.folder.map((value, key) => {
-        console.log("aqaaaasssss", value.value);
-        //console.log(value);
-      })
-    })
+    console.log("did update", this.state.names[0].children[0].value, "+++", this.state.names[1].children[0].value);
+    console.log(this.state.test)
   };
 
   render() {
     /*
-
     const names = this.state.names.map((name, key) => {
       return (
         <tr key={key}>
-
           <td>{name.value}
           </td>
           <td>Doe</td>
           <td>
             {name.value}
-
           </td>
           <td><TextModal/></td>
           <td><VideoModal/></td>
@@ -138,78 +154,9 @@ class Table extends Component {
     });
     return (
       <div className="TableScroll">
-        <button onClick={this.directories}>a</button>
-        <br />
-        <Panel id="collapsible-panel-example-2" defaultExpanded>
-          <Panel.Heading>
-            <Panel.Title toggle  onClick={() => this.setState({ open: !this.state.open })}>
-              Title that functions as a collapse toggle
-            </Panel.Title>
-          </Panel.Heading>
-          <Panel.Collapse>
-            <Panel.Body>
-              Anim pariatur cliche reprehenderit, enim eiusmod high life
-              accusamus terry richardson ad squid. Nihil anim keffiyeh
-              helvetica, craft beer labore wes anderson cred nesciunt sapiente
-              ea proident.
-            </Panel.Body>
-          </Panel.Collapse>
-        </Panel>
-        <PanelGroup accordion id="accordion-example">
-          <Panel eventKey="1">
-            <Panel.Heading>
-              <Panel.Title toggle onClick={() => this.setState({ open: !this.state.open })}>Collapsible Group Item #1</Panel.Title>
-              {console.log(this.state.open)}
-            </Panel.Heading>
-            <Panel.Body collapsible>
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-              richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-              dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-              moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-              assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-              wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-              butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-              aesthetic synth nesciunt you probably haven't heard of them accusamus
-              labore sustainable VHS.
-            </Panel.Body>
-          </Panel>
-          <Panel eventKey="2">
-            <Panel.Heading>
-              <Panel.Title toggle>Collapsible Group Item #2</Panel.Title>
-            </Panel.Heading>
-            <Panel.Body collapsible>
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-              richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-              dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-              moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-              assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-              wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-              butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-              aesthetic synth nesciunt you probably haven't heard of them accusamus
-              labore sustainable VHS.
-            </Panel.Body>
-          </Panel>
-          <Panel eventKey="3">
-            <Panel.Heading>
-              <Panel.Title toggle>Collapsible Group Item #3</Panel.Title>
-            </Panel.Heading>
-            <Panel.Body collapsible>
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
-              richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
-              dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
-              moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
-              assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-              wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
-              butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
-              aesthetic synth nesciunt you probably haven't heard of them accusamus
-              labore sustainable VHS.
-            </Panel.Body>
-          </Panel>
-        </PanelGroup>;
         <p>Or use a Panel.Toggle component to customize</p>
         <table className="table table-hover ">
           <thead>
-
           <tr>
             <th>Firstname</th>
             <th>Lastname</th>
@@ -224,14 +171,13 @@ class Table extends Component {
         </table>
       </div>
     );*/
-
     return (
-      <div style={{margin: 20, width: 400}}>
+      <div style={{margin: 20, width: 700}}>
         <Collapse
           accordion={true}
           onChange={this.onChange}
         >
-          {this.getItems()}
+          {p}
         </Collapse>
       </div>)
   }
--- src/components/Home/VideoModal.js
+++ src/components/Home/VideoModal.js
@@ -19,9 +19,10 @@ class Video_Modal extends Component {
   }
 
   render() {
+    let url = "https://interviewbotstorage.file.core.windows.net/interviews/pcass78@gmail.com/Test_7_4_2018_23_0_26/interview.webm?sv=2017-11-09&ss=bfqt&srt=sco&sp=rwdlacup&se=2018-12-06T10:06:04Z&st=2018-08-25T02:06:04Z&spr=https&sig=5AGJ1NaX6JM97J167OUqXqWme3k1cLyvS%2Fu5wUqfKo4%3D";
     return (
       <div>
-        <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId='L61p2uyiMSo'
+        <ModalVideo isOpen={this.state.isOpen} videoId='L61p2uyiMSo'
                     onClose={() => this.setState({isOpen: false})}/>
         <a onClick={this.openModal} href="\#">Video</a>
       </div>
