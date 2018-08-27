5defe0f3952c195bed46c8cd277d086793fbcd25
Daniel Francisco Soto Quesada <dsoto-as@sca-ws366.avantek-sc.avantica.avanticatec.net avantica.avanticatec.net avanticape.avantica.avanticatec.net avanticabol.avantica.avanticatec.net avantica.net>
2018-08-27 16:39:27 -0600

    prueba

--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -5,10 +5,6 @@ import {withRouter} from "react-router-dom";
 
 class Header extends Component {
 
-  constructor(props) {
-    super(props);
-  }
-
   handleLogout = () => {
     localStorage.clear();
     this.props.history.push("/login");
